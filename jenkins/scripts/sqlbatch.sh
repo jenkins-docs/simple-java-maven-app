1454f87213172f4aa88946c3fc6f8837974251b1
Ibrahim Alibrahim <ibrahim.alibrahim@amadeus.com>
2017-10-31 15:03:47 +0300

    vvf

--- jenkins/scripts/sqlbatch.sh
+++ jenkins/scripts/sqlbatch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sqlplus -s alms_ah/alms_ah@AMADEVDB03 << EOF
+/c/oraclexe/app/oracle/product/11.2.0/server/bin/sqlplus -s alms_ah/alms_ah@AMADEVDB03 << EOF
 whenever sqlerror exit sql.sqlcode;
 set echo off 
 set heading off
