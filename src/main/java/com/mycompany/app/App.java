21c70815bbd35a89c9a1fda06c5eae9d0d1567ee
4srinivas <4srinivas@gmail.com>
2017-11-15 13:18:40 -0500

    learning path
    Learning how to use github

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World second time!" );
     }
 }
