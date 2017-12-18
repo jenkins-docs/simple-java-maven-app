ecc4ab5d81fdcf5e47d82fa8593400daa350f061
Giles Gaskell <gilesgas@gmail.com>
2017-12-18 18:54:20 +1100

    Refactor App and made AppTest properly test App.
    * Update version of junit dependency in 'pom.xml' file (to 4.11) so that
    org.junit dependencies in AppTest could be imported.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,12 +2,20 @@ package com.mycompany.app;
 
 /**
  * Hello world!
- *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
     }
+
+    private final String getMessage() {
+        return message;
+    }
+
 }
