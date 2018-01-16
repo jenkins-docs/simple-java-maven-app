eb2bbf5301942c39a8a289cbe16c48afd5e01068
James <muguira.james@gmail.com>
2018-01-16 18:05:13 -0500

    add some tests3

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World: James!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World: James!\"");
+            fail("\"message\" is not \"Hello World!\"");
         }
     }
 
