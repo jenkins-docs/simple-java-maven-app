dd1ab142de8e9f8a4b7e393340051f4e2f8b67ff
James <muguira.james@gmail.com>
2018-01-18 11:59:44 -0500

    once more...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Bob!";
+    private final String message = "Hello Bobby!";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Bob!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Bobby!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Bob!\"");
+            fail("\"message\" is not \"Hello Bobby!\"");
         }
     }
 
