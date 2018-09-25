783ef3fbd75a1c8db9c4ae3960b229dc878f3795
Stanislau Sindalouski <stanislau_sindalouski@epam.com>
2018-09-25 12:50:05 +0300

    jenkins file moved to root

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,17 +40,6 @@ public class AppTest
         }
     }
 
-    @Test
-    public void testAppMain2()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
-    }
-
     @After
     public void cleanUpStreams() {
         System.setOut(null);
