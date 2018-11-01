4b4d787f6ce94eabc968c3de88a67a97bba8dbee
nikhilramavath <nikhil@mantralabsglobal.com>
2018-11-01 14:53:24 +0530

    bug fixes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World this is jenkin test!";
+    private final String message = "Hello World this is jenkin test! ";
 
     public App() {}
 
