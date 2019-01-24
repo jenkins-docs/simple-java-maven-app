6ff53366943e82697e3bda535af21fbb7ae7dd46
Dilsha Dominic <dilshadominic5@gmail.com>
2019-01-24 14:45:31 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,9 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        while(true) {
+            //Infinite loop
+        }
     }
 
     private final String getMessage() {
