57c5308321c45203e69aeeb9dc4fb2c1e87b40ae
anup.punekar <anup.punekar@gmail.com>
2019-03-10 23:27:10 +0530

    Snapshot job
    Snapshot job

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Jar!";
+    private final String message = "Hello (Snapshot) Jar!";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Jar!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello (Snapshot) Jar!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Jar!\"");
+            fail("\"message\" is not \"Hello (Snapshot) Jar!\"");
         }
     }
 
