b414dca33afe0f063516b5182bf3a2cec37b2575
fungchun1121 <fungchun1121@gmail.com>
2019-03-17 19:15:39 +0800

    new pr

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Caspar";
+    private final String message = "Hello World! Caspar xxxx";
 
     public App() {}
 
