f211304b96e8abcaae15871a5397bc3edc8dd899
CyrielleB <cyrielle.rech@yahoo.fr>
2019-05-23 12:08:22 +0200

    Ajout d'un nouvel affichage sur la console

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("Je s'appelle Groot");
     }
 
     private final String getMessage() {
