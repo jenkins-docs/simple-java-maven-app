70dec153b84718331e15e6cc828c5dab73f8579b
lemonbirdy <zhouchangbirdy@gmail.com>
2019-05-29 17:45:16 -0700

    wowowowowowow

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Chang!";
+    private final String message = "Surprise Mother F**k!";
 
     public App() {}
 
