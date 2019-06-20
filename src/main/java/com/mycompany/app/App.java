1dfd6d5fa636e7137e89ddb45d82a889ab4c514b
ashatus <ashish.deshpande1@ibm.com>
2019-06-19 17:26:12 -0700

    print right

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("Hos is that?");
     }
 
     private final String getMessage() {
