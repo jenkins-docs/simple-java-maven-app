e219f3597c557321f58ca4c1e250500f0e92b3db
Varun Jain <varunsj18@gmail.com>
2019-12-05 22:48:03 -0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,6 +13,8 @@ public class App
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
+    
+    
 
     private final String getMessage() {
         return message;
