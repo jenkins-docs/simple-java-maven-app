1b5e49857214f6952bd78f5ea2aa6a14ec957ed3
David Lee <david.lee@ibm.com>
2019-12-19 19:17:04 -0500

    Update deliver.sh
    This commit is from Dev branch

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,6 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 echo 'Testing for commit ***'
+echo 'New statement from Dev branch'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
