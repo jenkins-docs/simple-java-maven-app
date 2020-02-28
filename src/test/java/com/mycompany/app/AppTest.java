800f5902fe600862f2e40a17dc00923881b39b88
gjulien <gustavejulien@laposte.net>
2020-02-28 09:38:20 +0100

    c
    d

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"Hello!\"");
         }
     }
 
