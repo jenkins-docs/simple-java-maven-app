6e92cd617fe7306fa3c2591b955a4adcb103d6b7
jocke <jocke@10.200.3.113>
2020-08-20 10:35:21 +0200

    Added tests

--- src/test/java/com/mycompany/app/GameTest.java
+++ src/test/java/com/mycompany/app/GameTest.java
@@ -1,10 +1,6 @@
 package com.mycompany.app;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.After;
 import static org.junit.Assert.*;
 
 /**
@@ -13,23 +9,62 @@ import static org.junit.Assert.*;
 public class GameTest
 {
 
-    //@Before
-
+	
+	@Test
+    public void testRoll() {
+        int dice = 0;
+        int high = 6;
+        int low = 1;
+        
+        try {
+        	Dice testDice = new Dice();
+        	for (int i = 0; i < 1000; i++) {
+        		testDice.roll();
+            	dice = testDice.getResult();
+            	if (dice > high) {
+					fail("Value too high");
+				}
+            	else if (dice < low) {
+					fail("Value too low");
+				}
+            	
+			}
+        } catch (Exception e) {
+            fail("Dice not thrown :/");
+        }
+	}
+	
     @Test
     public void sortArrayTest() {
-        int[] initialArray = new int[]{2, 3, 1, 5};
+        int[] initialArray = new int[]{2, 3, 1, 5, 4};
+        int[] expected = new int[] {1, 2, 3 ,4 ,5};
         try {
-            System.out.println(initialArray);
-            assertEquals("123", "123");
-            //Round.sortArray(initialArray);
+        	Round round = new Round(null); //This will print "result of roll" but don't mind that
+            initialArray = round.sortArray(initialArray);
+            
+            assertArrayEquals(expected, initialArray);
         } catch (Exception e) {
             fail("Array was not sorted");
         }
     }
-
-//    @After
-//    public void cleanUpStreams() {
-//        System.setOut(null);
-//    }
-
+    @Test
+    public void testScoreOnes() {
+    	
+        int[] dices = new int[] {1, 1, 3 ,4 ,5};
+        int result;
+        int expected = 2;
+        
+        try {
+        	Score score = new Score();
+        	
+          	score.setScore(1,dices);
+          	result = score.getScore(1);
+          	
+          	assertEquals(expected, result);
+          	
+        } catch (Exception e) {
+            fail("Array was not sorted");
+        }
+    }
+    
 }
