34e4a365f09e8a05ea880b1e9cb41a4d04586d37
bigalasiva <sivakumarroyal1515@gmail.com>
2021-05-02 12:06:05 +0530

    removed ! from helloworld

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World Murali!";
+    private final String message = "Hello World Murali";
 
     public App() {}
 
