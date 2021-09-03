97cce91b9d4226cc0bd30bd6e8a0e3a32f46ea97
tpouche94 <tpouche94>
2021-09-03 21:26:23 +0000

    edite 2 files

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World! from Jordan to you\"");
+            fail("\"message\" is not \"Hello World!\"");
 	    
         }
     }
