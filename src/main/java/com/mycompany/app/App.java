1ce6a804f1eca11019f9b02d5bb561cd57d30db6
beluga_777 <82512132+rahulmalewadi@users.noreply.github.com>
2021-09-19 13:46:41 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,14 +3,14 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
+public class App{
 
-    private final String message = "Hello World!";
+     
+    private final String message = "Hello World!";//CLASS APP
 
-    public App() {}
+    public App() { } //APP FOR HELLOWORLD
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {          //Main method
         System.out.println(new App().getMessage());
     }
 
