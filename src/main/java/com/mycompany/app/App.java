ab8fb7d0009344de6da988977be1dc901fb26dcb
tpouche94 <jordanjoseph94@yahoo.com>
2021-12-16 12:02:14 -0500

    edit app.java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello nice  peeps";
+    private final String message = "Hello nice folks";
 
     public App() {}
 
