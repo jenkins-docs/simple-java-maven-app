d4c661661aac2594810adc5b71877a20251758b4
venkat <venkatn087@gmail.com>
2022-02-07 10:57:12 +0530

    formatter added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,17 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
-public class App
-{
+/** Hello world! */
+public class App {
 
-    private final String message = "Hello World!";
+  private final String message = "Hello World!";
 
-    public App() {}
+  public App() {}
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getMessage());
+  }
 
-    private final String getMessage() {
-        return message;
-    }
-
+  private final String getMessage() {
+    return message;
+  }
 }
