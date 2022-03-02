c0a7ac259f19f9636d709f54741df27b271d95d9
Mark Borrow <99207355+shiftleft-mark@users.noreply.github.com>
2022-03-02 12:46:52 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,6 +3,18 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
+
+public class CryptoUtil {
+
+    private readonly static string password = "P@%5w0r]>"; /*Literal*/
+
+    private static int ivLengthInBytes = 16;
+
+    private static int keyLengthInBits = 128;
+ 
+    private static int KeyLengthInBytes
+}
+
 public class App
 {
 
