a519179362c2320109b6a01017215da99932e37a
Mark Borrow <99207355+shiftleft-mark@users.noreply.github.com>
2022-03-02 13:02:23 -0500

    Create CryptoUtil.java

---
+++ src/main/java/com/mycompany/app/CryptoUtil.java
@@ -0,0 +1,11 @@
+public class CryptoUtil 
+{
+
+    private static String password = "P@%5w0r]>"; /*Literal*/
+
+    private static int ivLengthInBytes = 16;
+
+    private static int keyLengthInBits = 128;
+ 
+    private static int KeyLengthInBytes;
+}
