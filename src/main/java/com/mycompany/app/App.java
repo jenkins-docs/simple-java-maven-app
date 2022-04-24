7c3b7e1b820456951dd0fb102f8836e0a8dc4a24
Elod Molnar <elod.molnar@gmail.com>
2022-04-24 14:53:28 +0300

    example commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("test");
     }
 
     private final String getMessage() {
