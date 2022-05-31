a7fcfee61983ca74468bd36be2d5ed9bf6927088
bkapc2009i1 <102739932+bkapc2009i1@users.noreply.github.com>
2022-05-31 13:58:12 +0700

    commit test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -14,7 +14,7 @@ public class App
     public static void main(String[] args) {
         String test = new App().getMessage();
         System.out.println(new App().getMessage());
-        System.out.println(test);
+        System.out.println(test + "\r\n");
     }
 
     private final String getMessage() {
