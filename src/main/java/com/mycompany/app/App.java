5f6f58d7a5a8619ced571a46943fb67615382b0b
bkapc2009i1 <102739932+bkapc2009i1@users.noreply.github.com>
2022-05-31 13:29:30 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("hello");
     }
 
     private final String getMessage() {
