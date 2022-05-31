bbec47598bb1b9cbbf7968ee8b655cc677a3dfa0
bkapc2009i1 <102739932+bkapc2009i1@users.noreply.github.com>
2022-05-31 13:59:19 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -14,7 +14,7 @@ public class App
     public static void main(String[] args) {
         String test = new App().getMessage();
         System.out.println(new App().getMessage());
-        System.out.println(test + "\r\n");
+        System.out.println(test + System.getProperty("line.separator"));
     }
 
     private final String getMessage() {
