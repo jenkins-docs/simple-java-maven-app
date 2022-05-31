44613637a069001c0261051901c18a75f203b4ce
bkapc2009i1 <102739932+bkapc2009i1@users.noreply.github.com>
2022-05-31 13:35:13 +0700

    update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-         System.out.println("Hello World!");
+        System.out.println(new App().getMessage());
     }
 
     private final String getMessage() {
