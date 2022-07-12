4170b47ab19bc264f07191850ad101fd5a0350be
gustavozcdev <34316048+gustavozcdev@users.noreply.github.com>
2022-07-12 11:20:08 -0500

    Update build.sh

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-
+set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 echo `NAME: ${NAME}`
+set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 echo `VERSION: ${VERSION}`
 
