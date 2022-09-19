934a922e205acb266f2d0cdb69d95288dfc0852a
Hyedra64 <92393082+Hyedra64@users.noreply.github.com>
2022-09-20 00:08:38 +0700

    Update AppTest.java
    test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,28 +3,11 @@ package com.mycompany.app;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.apache.log4j.Logger;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 public class AppTest {
   
 	private Logger log = Logger.getLogger(this.getClass());
-
-    @BeforeAll
-    static void initAll() {
-    }
-
-    @BeforeEach
-    void init() {
-    }
   @Test  
-	@DisplayName("main")
     public void main(){  
 		try {
 			log.info("Starting execution of main");
@@ -33,19 +16,12 @@ public class AppTest {
  ;
  App app  =new App(); 
 app.main( args);
-Assertions.assertTrue(true);
+assertTrue(true);
 
 		} catch (Exception exception) {
 			log.error("Exception in execution ofmain-"+exception,exception);
 			exception.printStackTrace();
-			Assertions.assertFalse(false);
+			assertFalse(false);
 		}
     }  
-    @AfterEach
-    void tearDown() {
-    }
-
-    @AfterAll
-    static void tearDownAll() {
-    }
 }
