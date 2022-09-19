ff7e7a404e9cb17ee833c16355b19bee033e9c00
Hyedra64 <92393082+Hyedra64@users.noreply.github.com>
2022-09-20 00:06:25 +0700

    Update AppTest.java
    test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,17 +1,51 @@
 package com.mycompany.app;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.apache.log4j.Logger;
+import static org.junit.jupiter.api.Assertions.fail;
 
-public class Runner {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(ManageBeanTest.class);
-		
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
-		
-      System.out.println(result.wasSuccessful());
-   }
-}  
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+  
+	private Logger log = Logger.getLogger(this.getClass());
+
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+  @Test  
+	@DisplayName("main")
+    public void main(){  
+		try {
+			log.info("Starting execution of main");
+ String[] args = null; 
+;
+ ;
+ App app  =new App(); 
+app.main( args);
+Assertions.assertTrue(true);
+
+		} catch (Exception exception) {
+			log.error("Exception in execution ofmain-"+exception,exception);
+			exception.printStackTrace();
+			Assertions.assertFalse(false);
+		}
+    }  
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+}
