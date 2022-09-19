38a00aeb981156b1ae53b16c2e7a919cfe36761d
Hyedra64 <92393082+Hyedra64@users.noreply.github.com>
2022-09-19 18:46:50 +0700

    Update App.java
    Star Pattern

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,58 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
 public class App
 {
-
-    private final String message = "Hello World!";
-
-    public App() {}
-
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
 
-    private final String getMessage() {
-        return message;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+
+        int rows = sc.nextInt(); //Input
+
+//Upper Inverted Pyramid
+
+        for (int i = 0; i <= rows - 1; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                System.out.print(" "); //Print blank space
+
+            }
+
+            for (int k = i; k <= rows - 1; k++) {
+
+                System.out.print("*" + " "); //Print star and blank space
+
+            }
+
+            System.out.println(""); //New line
+
+        }
+
+//For lower Pyramid
+
+        for (int i = rows - 1; i >= 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+
+                System.out.print(" "); //Print spaces
+
+            }
+
+            for (int k = i; k <= rows - 1; k++) {
+
+                System.out.print("*" + " "); //Print Star and Space
+
+            }
+
+            System.out.println(""); //Print New line
+
+        }
+
+        sc.close();
+
     }
 
 }
