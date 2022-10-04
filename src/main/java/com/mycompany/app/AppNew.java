716684df79a617ece037f08f95c4724bcf1b2aec
Juan Manuel Bustillo Lorenzo <74539187+jbustillolorenzo@users.noreply.github.com>
2022-10-04 12:32:27 +0200

    Added new files

---
+++ src/main/java/com/mycompany/app/AppNew.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
