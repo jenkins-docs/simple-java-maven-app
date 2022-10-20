b2fadd5b000af14f488fc11caec9dabc0dc91693
Heimar Vega <heimarvega@gmail.com>
2022-10-20 08:45:15 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World 2021!";
+    private final String message = "Hello World 2022!";
 
     public App() {}
 
