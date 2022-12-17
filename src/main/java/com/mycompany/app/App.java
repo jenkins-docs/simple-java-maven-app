8f12cca2795c4c527073c9a3eeab519e08caaf89
Bhargavi Rajanala <bhargavirajanala97@gmail.com>
2022-12-17 19:48:28 +0530

    my duplicate commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World bhavana!";
+    private final String message = "Hello World bhargavi!";
 
     public App() {}
 
