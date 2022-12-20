c90694596dc160897eece55b3965a099dc180892
Mark Waite <mark.earl.waite@gmail.com>
2022-12-19 17:45:37 -0700

    Simplify tests

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,16 +5,15 @@ package com.mycompany.app;
  */
 public class App {
 
-    private final String message = "Hello World!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        System.out.println(MESSAGE);
     }
 
-    private final String getMessage() {
-        return message;
+    public String getMessage() {
+        return MESSAGE;
     }
-
 }
