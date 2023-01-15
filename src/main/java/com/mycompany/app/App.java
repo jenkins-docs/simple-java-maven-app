f7e5520b39a723babfd56a5373bcd5dfa9f7a780
Shubham Sharma <38440523+sudoshubh@users.noreply.github.com>
2023-01-15 16:03:55 +0530

    Update App.java
    Follow the standards of Java semantics in variable declarations .

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,15 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String message = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        System.out.println(message);
     }
 
     public String getMessage() {
-        return MESSAGE;
+        return message;
     }
 }
