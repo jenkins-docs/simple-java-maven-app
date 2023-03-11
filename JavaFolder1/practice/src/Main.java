3e3ca6b6c0ace13d97ef85512e02019ef79fc489
Md. Ibne Sina <md.ibnesina@Mds-MacBook-Air.local>
2023-03-11 11:30:45 +0600

    JavaFolder1 added

---
+++ JavaFolder1/practice/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        students s1 = new students("Sina", 1907002, 3.67);
+        System.out.println(s1.name + " " + s1.roll + " " + s1.cgpa);
+    }
+}
---
+++ JavaFolder1/practice/src/students.java
@@ -0,0 +1,35 @@
+public class students {
+    String name;
+    int roll;
+    double cgpa;
+
+    public students(String name, int roll, double cgpa) {
+        this.name = name;
+        this.roll = roll;
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+}
