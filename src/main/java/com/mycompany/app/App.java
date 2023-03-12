7e6ecc1d843c940bb1c57f60048ebc97099f9b2d
rk365040 <rknaidu08@gmail.com>
2023-03-11 21:37:04 -0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello!";
 
     public App() {}
 
