7d51f8d29023bfaf91c4753e5750c7ce103778bb
Aleksei Raguzin <a.raguzin83@gmail.com>
2023-03-13 20:13:37 +0300

    add services

---
+++ apps/hello-devops/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
