e024fcee2165ac4be88dcb9d13bdeb75d197d882
Aleksei Raguzin <a.raguzin83@gmail.com>
2023-03-13 19:49:11 +0300

    test2

--- apps/hello-world/src/test/java/com/mycompany/app/AppTest.java
+++ apps/hello-world/src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!!!", app.getMessage());
+        assertEquals("Hello World!", app.getMessage());
     }
 }
