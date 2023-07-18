3abda22706835de0cd25719280d734b4bd57c70e
Elod Molnar <elod.molnar@gmail.com>
2023-07-18 15:14:07 +0300

    readme

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,6 +2,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
 @RestController
 @EnableAutoConfiguration
 public class Example {
@@ -9,6 +11,11 @@ public class Example {
     @RequestMapping("/hello-world")
     String home() {
         return "Hello World - v5!";
+    }
+
+    @RequestMapping("/hello-world/date")
+    String date() {
+        return new Date().toString();
     }
 
     public static void main(String[] args) {
