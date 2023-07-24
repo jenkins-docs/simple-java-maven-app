aa488dba6d56a8b3e314202d02b60e0c437f71a0
rakesh635 <rakesh635@gmail.com>
2023-07-24 10:19:33 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World123!";
+    private static final String MESSAGE = "Hello World1234!";
 
     public App() {}
 
