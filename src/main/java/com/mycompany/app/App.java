01d940a55f99293b7b3db21f295f5a84d23850e1
student <vikashkumarjain@gmail.com>
2023-08-20 14:38:47 +0000

    Initial test changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,6 +11,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        System.out.println("hello");
+
     }
 
     public String getMessage() {
