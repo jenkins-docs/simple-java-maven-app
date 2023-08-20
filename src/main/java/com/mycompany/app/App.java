f65d6f003d84bdbb747d3f8e8dc3e7d8b73aabcf
student <vikashkumarjain@gmail.com>
2023-08-20 14:41:46 +0000

    2 commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         System.out.println("hello");
+        System.out.println("hello1");
 
     }
 
