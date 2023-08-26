6b9249948bada4af7af29a81698dc93764ca57c6
LifeMaste <134485337+LifeMaste@users.noreply.github.com>
2023-08-26 20:59:54 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
          System.out.println(Hello-world1);
+          System.out.println(Hello-world23);
     }
 
     public String getMessage() {
