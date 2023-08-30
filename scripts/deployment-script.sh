bf003d228e8de430ec77dec09619270b3517b8d9
roy.bidani <roybidani2@gmail.com>
2023-08-30 16:25:56 +0300

    deploy on ec2 instance

--- scripts/deployment-script.sh
+++ scripts/deployment-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define Docker image details
-DOCKER_IMAGE="roybidani/simple-java-maven-app:latest"
+DOCKER_IMAGE="roybidani/simple-java-maven-app:${{ github.sha }}"
 # Pull the latest Docker image
 docker pull $DOCKER_IMAGE
 # Stop and remove the existing container (if it exists)
@@ -8,5 +8,6 @@ docker stop simple-java-maven-app || true
 docker rm simple-java-maven-app || true
 # Run the Docker container
 docker run -d --name simple-java-maven-app -p 80:8080 $DOCKER_IMAGE
+
 
 
