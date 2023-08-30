927a76c6255574c60462c026a2d1bef4b2098037
roy.bidani <roybidani2@gmail.com>
2023-08-30 16:03:50 +0300

    deploy stage

---
+++ scripts/deployment-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Define Docker image details
+DOCKER_IMAGE="roybidani/simple-java-maven-app:latest"
+# Pull the latest Docker image
+docker pull $DOCKER_IMAGE
+# Stop and remove the existing container (if it exists)
+docker stop simple-java-maven-app || true
+docker rm simple-java-maven-app || true
+# Run the Docker container
+docker run -d --name simple-java-maven-app -p 80:8080 $DOCKER_IMAGE
+
+
