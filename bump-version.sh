a38d9b8e709e4e6ef4b46d0b5a04db56a85673e5
roy.bidani <roybidani2@gmail.com>
2023-08-30 14:09:51 +0300

    fixes 10

--- bump-version.sh
+++ bump-version.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Set Git user identity for this repository
+git config user.email "you@example.com"
+git config user.name "Your Name"
+
 current_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 new_version=$(echo $current_version | awk -F. '{$3++; OFS="."; print $0}')
 mvn versions:set -DnewVersion=$new_version
 git commit -am "Bump patch version"
 git push origin main
+
