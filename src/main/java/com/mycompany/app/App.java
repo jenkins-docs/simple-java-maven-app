c189e4c275460ad520b5c03bb40aafcbfcb37f0f
Midfielder84 <129120286+Midfielder84@users.noreply.github.com>
2023-09-01 16:34:07 +0200

    Update App.java - add new line

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         System.out.println("Hello Midfielder");
+        System.out.println("New changes in new branch");
     }
 
     public String getMessage() {
