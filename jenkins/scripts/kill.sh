689f2dc91ef478be59498ab678788bb5fffa5ef4
yogypratama <142618324+yogypratama@users.noreply.github.com>
2023-09-27 09:14:38 +0800

    Add files via upload

---
+++ jenkins/scripts/kill.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+echo 'The following command terminates the "npm start" process using its PID'
+echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
+echo 'was executed.'
+set -x
+kill $(cat .pidfile)
