944ef6d899cfe726da207fcd5af15ae3855627ba
yogypratama <142618324+yogypratama@users.noreply.github.com>
2023-09-27 09:45:19 +0800

    Delete jenkins/scripts/kill.sh

--- jenkins/scripts/kill.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
