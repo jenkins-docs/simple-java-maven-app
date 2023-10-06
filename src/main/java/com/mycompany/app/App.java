9b5b6ae74b7270b76dddac1bcf52536bb78ae4d2
Panos Panagiotopoulos <panospanos570@gmail.com>
2023-10-06 16:55:59 +0300

    Changed to Swing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,32 @@
 package com.mycompany.app;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Hello world!
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "\tHello World!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        JFrame window = new JFrame();
+        window.setSize(700,700);
+        window.getContentPane().setBackground(Color.LIGHT_GRAY);
+        window.setResizable(false);
+        window.setLayout(null);
+
+        JTextField message = new JTextField(MESSAGE);
+        message.setBounds(210 , 210 , 250 , 250);
+        message.setBackground(Color.DARK_GRAY);
+        message.setForeground(Color.WHITE);
+        message.setVisible(true);
+
+        window.add(message);
+        window.setVisible(true);
     }
 
-    public String getMessage() {
-        return MESSAGE;
-    }
 }
