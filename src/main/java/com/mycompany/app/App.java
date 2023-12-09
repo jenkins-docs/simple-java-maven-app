12b9658089dafb5f93f8bc3e4832a5638c3fbbdf
venkatn087 <52975559+venkatn087@users.noreply.github.com>
2023-12-09 20:13:41 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,8 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println(new App().getMessage());
+    //System.out.println(new App().getMessage());
+    //var i=0;
   }
 
   private final String getMessage() {
