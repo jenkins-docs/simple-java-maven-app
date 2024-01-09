44ec0a92858461b0bb10f628b76844ba8191b267
Bassim94 <106307304+Bassim94@users.noreply.github.com>
2024-01-09 14:30:32 +0300

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,4 +1,4 @@
-/usr/bin/env bash
+#!/usr/bin/env bash
 
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
