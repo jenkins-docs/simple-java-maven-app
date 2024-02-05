7a4841c117090209d647362262515d4392ad2a91
gurumurthygd <gurumurthygd12@gmail.com>
2024-02-05 22:50:56 +0530

    first commit

---
+++ src/main/java/com/mycompany/app/App2.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App2 {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App2() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
