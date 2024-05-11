f83172fe29b287a55c8247a3071873da604799bf
Yossi <yosefshalumov@gmail.com>
2024-05-11 14:58:07 +0300

    profile assoc was missing ;(

--- terraform/ssm_role/main.tf
+++ terraform/ssm_role/main.tf
@@ -28,3 +28,8 @@ resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = data.aws_iam_policy.ssm_managed_instance_core.arn
 }
+
+resource "aws_iam_instance_profile_association" "ssm_profile_association" {
+  instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
+  role             = aws_iam_role.ssm_role.name
+}
