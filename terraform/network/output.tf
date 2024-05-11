99ab465fcf89d89801c804be6f935d1cf9261ce1
Yossi <yosefshalumov@gmail.com>
2024-05-11 13:58:41 +0300

    public/private :(

--- terraform/network/output.tf
+++ terraform/network/output.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "private_sub_id" {
-  value = aws_subnet.public.id
+  value = aws_subnet.private.id
 }
 
 output "public_sub_id" {
-  value = aws_subnet.private.id
+  value = aws_subnet.public.id
 }
