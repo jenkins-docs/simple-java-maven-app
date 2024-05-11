3d38b3b88a8df00f564abe8df33ec89d254902e4
Romi.Geora <Romi29396@gmail.com>
2024-05-11 09:06:29 +0300

    Terraform files added

--- create_instance.tf
+++ create_instance.tf
@@ -3,11 +3,11 @@ provider "aws" {
     region     = "us-east-1"
 }
 
-resource "aws_instance" "terraform_server" {
+resource "aws_instance" "maven_github_server" {
     ami           = "ami-080e1f13689e07408"
     instance_type = "t2.micro"
 
     tags = {
-        Name = "Terraform"
+        Name = "Maven_GitHub_Actions"
     }
 }
