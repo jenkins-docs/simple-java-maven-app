d20fb681e87251d6cdff46f25abaa8fa5e516d0b
Romi.Geora <Romi29396@gmail.com>
2024-05-12 17:45:49 +0300

    some fixes

--- TF/create_instance.tf
+++ TF/create_instance.tf
@@ -5,8 +5,10 @@ provider "aws" {
 resource "aws_instance" "maven_github_server" {
     ami           = "ami-080e1f13689e07408"
     instance_type = "t2.micro"
+    security_groups = [ "sg-0755089add435653a" ]
 
     tags = {
         Name = "Maven_GitHub_Actions"
     }
 }
+
