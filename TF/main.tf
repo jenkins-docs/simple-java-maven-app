4b53e5b939bec5393dca073248e682cf441cf11b
Romi.Geora <Romi29396@gmail.com>
2024-05-12 21:40:01 +0300

    some fixes

--- TF/main.tf
+++ TF/main.tf
@@ -17,7 +17,6 @@ resource "aws_instance" "maven_github_actions_server" {
               sudo groupadd docker
               sudo usermod -aG docker ubuntu
               sudo docker run -d -p 8000:8000 romi293/java_github_actions:latest
-              #sudo docker run -d -p 8000:8000 romi293/java_github_actions:${var.docker_image_version}
               EOF
 
   tags = {
