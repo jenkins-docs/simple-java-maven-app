fd2c98ecc0e81c2fbf3a9bcde9117b44e6571d85
Ziv <95753868+Zivgl66@users.noreply.github.com>
2024-05-12 08:40:19 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-  backend "s3" {
+ 
+  }
+ backend "s3" {
     bucket                  = "ziv-tf-state"
     key                     = "my-terraform-project"
     region                  = "us-east-1"
     }
-  }
-
   required_version = ">= 1.2.0"
 }
 
