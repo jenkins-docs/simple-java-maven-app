1e95e621eb45912ac18cee5d7b7e8fae5d51dd27
Romi.Geora <Romi29396@gmail.com>
2024-05-13 11:34:16 +0300

    bug

--- TF/main.tf
+++ TF/main.tf
@@ -7,6 +7,8 @@ provider "aws" {
     region = "us-east-1"
 }
 
+##################################################################################
+
 resource "aws_instance" "maven_github_actions_server" {
   ami                    = "ami-080e1f13689e07408"
   instance_type          = "t2.micro"
@@ -28,6 +30,27 @@ resource "aws_instance" "maven_github_actions_server" {
     Name = "Maven_GitHub_Actions1"
   }
 }
+
+##################################################################################
+
+resource "aws_eks_cluster" "demo" {
+  name     = "my_cluster"
+  role_arn = aws_iam_role.demo.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
+}
+
+
+
 
 
 
