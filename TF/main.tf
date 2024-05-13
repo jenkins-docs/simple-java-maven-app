878c9fd9aa6d22e546f9a88f3c85995aeb33bfc7
Romi.Geora <Romi29396@gmail.com>
2024-05-13 10:18:31 +0300

    some fixes

--- TF/main.tf
+++ TF/main.tf
@@ -20,8 +20,8 @@ resource "aws_instance" "maven_github_actions_server" {
               sudo systemctl start docker
               sudo systemctl enable docker
               sudo groupadd docker
-              sudo usermod -aG docker ubuntu
-              sudo docker run -d -p 8000:8000 romi293/java_github_actions
+              sudo usermod -aG docker $(whoami)
+              sudo docker run -d -p 80:8000 romi293/java_github_actions
             EOF
 
   tags = {
