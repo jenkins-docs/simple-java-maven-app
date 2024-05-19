800e2e3bf4dc913094090b1f1f28b64ac7ce72a6
Girish <girishbalanagu@gmail.com>
2024-05-18 21:45:06 -0300

    Name and version regex updated

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,13 +11,15 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+# NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(mvn help:evaluate -Dexpression=project.name | grep -v "^\[" | sed 's/\x1b\[[0-9;]*m//g')
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+# VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep -E "^[0-9]+\.[0-9]+.*"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
