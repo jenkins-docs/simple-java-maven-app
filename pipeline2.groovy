cfd0857a4f472d5c4740e8a100fc3274ceaa9f65
Your Name <durgabhavani9031@gmail.com>
2024-06-19 15:41:28 -0400

    updateing pipeline2

---
+++ pipeline2.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone application code') {
+            steps {
+                git 'https://github.com/durga97176/simple-java-maven-app.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'mvn package -f pom.xml'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'deployed the application code'
+            }
+        }
+    }
+}
