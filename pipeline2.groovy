d9b8dab2a089293f5ba0251a0ff52e5673b346d5
durga97176 <durgabhavani9031@gmail.com>
2024-06-19 16:08:34 -0400

    updateing pipeline2

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -12,10 +12,13 @@ pipeline {
                 sh 'mvn package -f pom.xml'
             }
         }
+        stage('Build') {
+            steps {
+                echo 'mvn package -f pom.xml'
+            }
+        }
         stage('Deploy') {
             steps {
                 echo 'deployed the application code'
-            }
-        }
     }
 }
