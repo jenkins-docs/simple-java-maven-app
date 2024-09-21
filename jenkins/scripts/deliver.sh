ad23f102a31303bc264bfa72dc5319a6186ea3e0
Balasai Kolli <balasaikolli348@gmail.com>
2024-09-21 18:01:21 +0530

    jenkins-test file update

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
+echo "The following Maven command installs your Maven-built Java application"
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
