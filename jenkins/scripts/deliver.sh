8cdf2f5cc1a63dca3380868b00495bf65558a582
hari <bukkehari789@gmail.com>
2024-10-06 17:37:33 +0530

    Revert "deleted jenkins file"
    This reverts commit d5d7ddc0923d964eacc01ce94f1412e778b5eea6.

---
+++ jenkins/scripts/deliver.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo 'The following Maven command installs your Maven-built Java application'
+echo 'into the local Maven repository, which will ultimately be stored in'
+echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
+echo 'volume).'
+set -x
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+set +x
+
+echo 'The following command extracts the value of the <name/> element'
+echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+set -x
+NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+set +x
+
+echo 'The following command behaves similarly to the previous one but'
+echo 'extracts the value of the <version/> element within <project/> instead.'
+set -x
+VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+set +x
+
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
+java -jar target/${NAME}-${VERSION}.jar
