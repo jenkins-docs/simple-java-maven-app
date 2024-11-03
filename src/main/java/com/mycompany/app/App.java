c691639dbb16006b30e30b17e2cc24c9a5f32c69
Javeed <Javeed>
2024-11-03 22:08:35 +0800

    Addimg

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,11 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-<<<<<<< HEAD
-    private final String message = "Hello World123!";
-=======
     private static final String MESSAGE = "Hello World!";
->>>>>>> c0293ed9625adf68934ae4b75973ad2c797060da
 
     public App() {}
 
