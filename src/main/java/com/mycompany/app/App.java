47137ef1af1a7e9d653e98f2085350af56768405
Ankush Chatterjee <ankush.chatterjee@harness.io>
2024-11-21 00:04:01 +0530

    Fail a test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World!123";
 
     public App() {}
 
