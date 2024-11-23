fac7c6a399939eee651f1caf3edad2ab43162d2b
WhyteTy <aldentes123+10@gmail.com>
2024-11-23 16:18:03 +0100

    add test message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,6 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
+        System.out.println("Message de test : ")
         System.out.println(MESSAGE);
     }
 
