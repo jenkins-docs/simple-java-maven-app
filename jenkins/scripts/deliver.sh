1039b1c10ef5c96c095883e1a45035950666b2c6
Jagadeesh999 <pjagadeesh999@gmail.com>
2024-11-24 13:20:44 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+chmod -R 777 target/*
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
@@ -23,7 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-
-chmod -R 777 target/*
 
 java -jar target/${NAME}-${VERSION}.jar
