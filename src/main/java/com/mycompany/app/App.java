b9b35a7b35bd4dac014810fced4e1efc12047821
NGomes1990 <93449942+NGomes1990@users.noreply.github.com>
2025-03-10 14:25:50 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,6 +4,8 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App {
+     Logger logger = Logger.getLogger(getClass().getName());
+
 
     private static final String MESSAGE = "Hello World!";
 
@@ -11,6 +13,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        logger.info("My Message");  // Compliant, output via logger
     }
 
     public String getMessage() {
