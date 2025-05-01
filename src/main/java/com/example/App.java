9ee817be66bece1258e0b4f7e8beecf5567cfe3c
devsecopslabs2025 <suraj.devsecops2025@gmail.com>
2025-05-01 10:25:14 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World! This is a Java Maven Tomcat application.");
+        System.out.println("Hello, World! This is a Java Maven Tomcat application - MultiBranch Pipeline.");
         // Additional application logic can be added here
     }
 }
