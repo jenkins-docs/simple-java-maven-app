f79b3d1a22199788d65a34c7e100cf007f0a6eff
yudi-corgi <chendongyu@kpaycn.local>
2025-05-13 17:15:14 +0800

    style: comment.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Dev!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
