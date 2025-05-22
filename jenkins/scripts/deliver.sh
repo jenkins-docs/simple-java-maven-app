8e5d75ef4051906b5800dcc7e3aeaf2a41d7ca47
Max Graef <70293952+graefmf@users.noreply.github.com>
2025-05-22 15:25:09 -0600

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,19 +5,19 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name -Dstyle.color=never
+mvn -Dstyle.color=never jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name -Dstyle.color=never`
+NAME=`mvn -Dstyle.color=never -q -DforceStdout help:evaluate -Dexpression=project.name -Dstyle.color=never`
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version -Dstyle.color=never`
+VERSION=`mvn -Dstyle.color=never -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
