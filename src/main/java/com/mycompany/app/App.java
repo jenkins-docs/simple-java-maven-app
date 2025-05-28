5ad85303de02e062e23f1f2d807f5e7ffb46f218
汪浩浩 <whh881114@gmail.com>
2025-05-28 11:15:21 +0800

    改造成spring-boot

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,19 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        SpringApplication.run(App.class, args);
     }
 
-    public String getMessage() {
-        return MESSAGE;
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
     }
 }
