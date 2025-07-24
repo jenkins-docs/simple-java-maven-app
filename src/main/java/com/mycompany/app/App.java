4a4cc8c28c1a68f0716cfa58f6dd5b843a2d4ba9
monish.wankhade <monish.wankhade@impetus.com>
2025-07-24 17:57:59 +0530

    Add p1, p2, p3 fields with getters/setters to App.java and corresponding unit tests in AppTest.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,13 +7,41 @@ public class App {
 
     private static final String MESSAGE = "Hello World!";
 
+    private String p1;
+    private String p2;
+    private String p3;
+
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
     }
 
-    public String getMessage() {
+    public String getsMessage() {
         return MESSAGE;
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+    public String getP2() {
+        return p2;
+    }
+
+    public void setP2(String p2) {
+        this.p2 = p2;
+    }
+
+    public String getP3() {
+        return p3;
+    }
+
+    public void setP3(String p3) {
+        this.p3 = p3;
     }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -22,4 +22,25 @@ public class AppTest
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
     }
+
+    @Test
+    public void testP1GetterSetter() {
+        App app = new App();
+        app.setP1("value1");
+        assertEquals("value1", app.getP1());
+    }
+
+    @Test
+    public void testP2GetterSetter() {
+        App app = new App();
+        app.setP2("value2");
+        assertEquals("value2", app.getP2());
+    }
+
+    @Test
+    public void testP3GetterSetter() {
+        App app = new App();
+        app.setP3("value3");
+        assertEquals("value3", app.getP3());
+    }
 }
