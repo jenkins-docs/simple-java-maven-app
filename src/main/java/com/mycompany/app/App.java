28e4304b618df3762721a9a720e88bfea0101002
Devindi Jayawardena <83491639+DevindiJayawardena@users.noreply.github.com>
2025-11-22 23:06:35 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,19 +3,19 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
+// public class App
+// {
 
-    private final String message = "Hello World!";
+//     private final String message = "Hello World!";
 
-    public App() {}
+//     public App() {}
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+//     public static void main(String[] args) {
+//         System.out.println(new App().getMessage());
+//     }
 
-    private final String getMessage() {
-        return message;
-    }
+//     private final String getMessage() {
+//         return message;
+//     }
 
 }
