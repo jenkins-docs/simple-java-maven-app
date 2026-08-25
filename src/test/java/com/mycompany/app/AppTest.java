e6c5f6621fef640bc3b0d9e472ce6b9990a537a6
YukthaGIT <abcdefgh4870@gmail.com>
2026-08-25 20:22:21 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!\nYuktha", app.getMessage());
+        assertEquals("Hello World!\nYuktha\n CI", app.getMessage());
     }
 }
